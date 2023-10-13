e84bafca5af8d61525f86a199ef35e91610ec7a4
Kota Matsudo <t21cs048@yamanashi.ac.jp>
2023-10-13 15:39:43 +0900

    リモートリポジトリ作成演習のため,新規ファイルをコミット

---
+++ hello.py
@@ -0,0 +1,20 @@
+'''
+Created on 2023/10/13
+
+@author: t21cs048
+'''
+#emptyテンプレートで挿入されるコメント
+
+print("Hello, Python world")
+
+a = 10
+b = 0.00001
+c = 'string'
+d = 10; e = 0.00001; f = "string"
+
+print(a,b,c)
+print(d,e,f)
+for x in {1,2,3}:
+    print(x)
+for x in range (3):
+    print(x+1)
